9e53b3061ebc810fb493ad90b9038089897adda9
zhangjingjia1 <zhangjingjia1@kingsoft.com>
2021-09-06 21:17:56 +0800

    【ADD】第一次提交NewTankWar代码

---
+++ NewTankWar/Assets/PrefabCreate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabCreate : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Random.Range(-10, 10);
+        float y = Random.Range(-10, 10);
+        float z = Random.Range(-10, 10);
+        Vector3 pos = new Vector3(x, y, z);
+
+        //Instantiate(prefab, pos, Quaternion.identity);
+    }
+}
